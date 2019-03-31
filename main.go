8bdf50d1e80c5baffe66e4b7d6b6fae4449733ed
sergei rkarimov <s.karimov@outlook.com>
2019-04-01 00:55:28 +0300

    Signup was done

--- main.go
+++ main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/lib/pq"
 
 	"github.com/gorilla/mux"
 )
 
+type Error struct {
+	Message string `json:"message"`
+}
+
+type User struct {
+	Id       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 var db *sql.DB
 
 func main() {
-	pgUrl, err := pq.ParseURL("postgres://hell:@localhost/aaa_db?sslmode=verify-full")
+	pgUrl, err := pq.ParseURL("postgres://hell:@localhost/aaa_db?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,8 +53,52 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Innopolis is the best."))
 }
 
+func respondWithError(w http.ResponseWriter, status int, error Error) {
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(error)
+}
+
+func respoonseJSON(w http.ResponseWriter, data interface{}) {
+	json.NewEncoder(w).Encode(data)
+}
+
 func signup(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Signup invoked.")
+	var user User
+	var error Error
+
+	json.NewDecoder(r.Body).Decode(&user)
+
+	if user.Email == "" {
+		error.Message = "Email is missing."
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	if user.Password == "" {
+		error.Message = "Password is missing."
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user.Password = string(hash)
+
+	stmt := "INSERT INTO users (email, password, ip) VALUES ($1, $2, $3) RETURNING id;"
+	err = db.QueryRow(stmt, user.Email, user.Password, r.RemoteAddr).Scan(user.Id)
+	if err != nil {
+		fmt.Println(err)
+		error.Message = "Server error"
+		respondWithError(w, http.StatusInternalServerError, error)
+		return
+	}
+
+	user.Password = ""
+	w.Header().Set("Content-Type", "application/json")
+	respoonseJSON(w, user)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
