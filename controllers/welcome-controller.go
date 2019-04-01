6ac5b83d305d8b16aeab9a603ac1749ae8a30b2f
sergei rkarimov <s.karimov@outlook.com>
2019-04-01 14:15:53 +0300

    All done

--- controllers/welcome-controller.go
+++ controllers/welcome-controller.go
@@ -1,10 +1,18 @@
 package controllers
 
-import "net/http"
-import u "../utils"
+import (
+	"encoding/json"
+	"net/http"
+
+	"../models"
+	u "../utils"
+)
 
 func (c Controller) ProtectedEndpoint() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		u.ResponseJSON(w, "Welcome to Innopolis.")
+		var user models.User
+
+		json.NewDecoder(r.Body).Decode(&user)
+		u.ResponseJSON(w, "Welcome "+user.Email+" to Innopolis.")
 	}
 }
