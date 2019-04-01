45df7eb34e56bf2f2768e532372a322bcb33d1b6
sergei rkarimov <s.karimov@outlook.com>
2019-04-01 10:11:40 +0300

    Task was done

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
 
 	"golang.org/x/crypto/bcrypt"
 
@@ -13,6 +16,10 @@ import (
 
 	"github.com/gorilla/mux"
 )
+
+type JWT struct {
+	Token string `json:"token"`
+}
 
 type Error struct {
 	Message string `json:"message"`
@@ -22,6 +29,7 @@ type User struct {
 	Id       int    `json:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+	Ip       string `json:"ip"`
 }
 
 var db *sql.DB
@@ -58,8 +66,22 @@ func respondWithError(w http.ResponseWriter, status int, error Error) {
 	json.NewEncoder(w).Encode(error)
 }
 
-func respoonseJSON(w http.ResponseWriter, data interface{}) {
+func responseJSON(w http.ResponseWriter, data interface{}) {
 	json.NewEncoder(w).Encode(data)
+}
+
+func GenerateToken(user User) (string, error) {
+	var err error
+	secret := "Innopolis is the Best"
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": user.Email,
+		"iss":   "innopolis",
+	})
+
+	tokenString, err := token.SignedString([]byte(secret))
+
+	return tokenString, err
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
@@ -88,7 +110,7 @@ func signup(w http.ResponseWriter, r *http.Request) {
 	user.Password = string(hash)
 
 	stmt := "INSERT INTO users (email, password, ip) VALUES ($1, $2, $3) RETURNING id;"
-	err = db.QueryRow(stmt, user.Email, user.Password, r.RemoteAddr).Scan(user.Id)
+	err = db.QueryRow(stmt, user.Email, user.Password, r.RemoteAddr).Scan(&user.Id)
 	if err != nil {
 		fmt.Println(err)
 		error.Message = "Server error"
@@ -98,18 +120,103 @@ func signup(w http.ResponseWriter, r *http.Request) {
 
 	user.Password = ""
 	w.Header().Set("Content-Type", "application/json")
-	respoonseJSON(w, user)
+	responseJSON(w, user)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Login invoked.")
+	var user User
+	var jwt JWT
+	var error Error
+
+	json.NewDecoder(r.Body).Decode(&user)
+
+	if user.Email == "" {
+		error.Message = "Email is missing."
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	if user.Password == "" {
+		error.Message = "Password is missing."
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	password := user.Password
+
+	row := db.QueryRow("select * from users where email=$1", user.Email)
+	err := row.Scan(&user.Id, &user.Email, &user.Password, &user.Ip)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			error.Message = "The user does not exist"
+			respondWithError(w, http.StatusBadRequest, error)
+			return
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	hashedPassword := user.Password
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+
+	if err != nil {
+		error.Message = "Invalid Password"
+		respondWithError(w, http.StatusUnauthorized, error)
+		return
+	}
+
+	token, err := GenerateToken(user)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	jwt.Token = token
+
+	responseJSON(w, jwt)
 }
 
 func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("ProtectedEndpoint invoked.")
+	w.Write([]byte("Welcome to Innopolis."))
 }
 
 func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
-	fmt.Println("TokenVerifyMiddleWare invoked.")
-	return nil
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var errorObject Error
+		authHeader := r.Header.Get("Authorization")
+		bearerToken := strings.Split(authHeader, " ")
+
+		if len(bearerToken) == 2 {
+			authToken := bearerToken[1]
+
+			token, error := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+
+				return []byte("Innopolis is the Best"), nil
+			})
+
+			if error != nil {
+				errorObject.Message = error.Error()
+				respondWithError(w, http.StatusUnauthorized, errorObject)
+				return
+			}
+
+			if token.Valid {
+				next.ServeHTTP(w, r)
+			} else {
+				errorObject.Message = error.Error()
+				respondWithError(w, http.StatusUnauthorized, errorObject)
+				return
+			}
+		} else {
+			errorObject.Message = "Invalid token."
+			respondWithError(w, http.StatusUnauthorized, errorObject)
+			return
+		}
+	})
 }
